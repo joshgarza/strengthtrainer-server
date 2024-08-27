8dd1db9fd83b65ff17ff2e78bf451276626cfe04
Josh <josh@sf-iron.com>
2024-08-26 17:26:51 -0700

    Implemented bcrypt hashing when registering and comparing against hash when logging in

--- src/controllers/userControllers.ts
+++ src/controllers/userControllers.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from "express";
 import { userModels } from "../models/index.js";
+import bcrypt from "bcrypt";
 
 export const userControllers = {
   test: async (req: Request, res: Response): Promise<void> => {
@@ -7,13 +8,36 @@ export const userControllers = {
   },
   register: async (req: Request, res: Response): Promise<void> => {
     console.log("Registering");
-    const name = "josh";
-    const email = "josh@josh-garza.com";
-    const passwordHash = "afsjkl34jw3890sf8a9";
-    const role = "coach";
-    res.send(userModels.register(name, email, passwordHash, role));
+    const { username, email, password, role } = req.body;
+    // run password through bcrypt and send the result of that as password hash to userModels.register
+    bcrypt.hash(password, 10, (err, hash) => {
+      console.log("PW:", password);
+      console.log("Hash:", hash);
+      res.send(userModels.register(username, email, hash, role));
+    });
   },
-  login: async (req: Request, res: Response): Promise<void> => {},
+  login: async (req: Request, res: Response): Promise<void> => {
+    console.log("Logging in");
+    const { email, password } = req.body;
+
+    // get user hash
+    const hash = await userModels.getUserHash(email);
+    console.log(hash);
+
+    bcrypt.compare(password, hash, (err, result) => {
+      if (err) {
+        console.log("Error logging in:", err);
+        res.send("Error logging in");
+      } else {
+        console.log("Result:", result);
+        if (result) {
+          res.send("Logged in.");
+        } else {
+          res.send("Error logging in");
+        }
+      }
+    });
+  },
   passwordReset: async (req: Request, res: Response): Promise<void> => {},
   resetPassword: async (req: Request, res: Response): Promise<void> => {},
 };
--- src/models/userModels.ts
+++ src/models/userModels.ts
@@ -7,12 +7,39 @@ export const userModels = {
   //     return result.rows[0].now;
   //   },
   // };
+  getUserHash: async (email: string) => {
+    const query = `SELECT password_hash FROM users WHERE email = $1`;
+    const value = [email];
+
+    try {
+      const res = await client.query(query, value);
+      return res.rows[0].password_hash;
+    } catch (err) {
+      console.error("Error executing query", err);
+      throw err;
+    }
+  },
   register: async (
     name: string,
     email: string,
     passwordHash: string,
     role: string
   ) => {
+    /**
+     * what's involved in registering?
+     * - checking to see if there's a conflict in the db for the email
+     * - email is unique
+     * -- email validation
+     * - hash the password
+     * - insert into users: name, email, hash, and role
+     */
+
+    // check unique email
+
+    // validate email
+
+    //
+
     const query = `INSERT INTO users (name, email, password_hash, role) VALUES ($1, $2, $3, $4) RETURNING *`;
     const values = [name, email, passwordHash, role];
 
@@ -28,3 +55,11 @@ export const userModels = {
   passwordReset: async () => {},
   resetPassword: async () => {},
 };
+
+/**
+ * thinking of this as a class instead of just a basic object
+ *
+ * userModels can have a constructor that initializes the client object in /config/db.js
+ * methods to do basic operations can be abstracted to a common class that can be used across various models
+ * maybe we have a generic
+ */
