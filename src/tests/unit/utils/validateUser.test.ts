507788a94c38dfb179a60bd514fc596a7a1921f5
Josh <josh@sf-iron.com>
2024-08-30 10:07:28 -0700

    Refactored routing. At middlewareSetup, we create an authRouter and openRouter, check the path and route accordingly.
    Wrote tests for validateUser on non POST requests.

---
+++ src/tests/unit/utils/validateUser.test.ts
@@ -0,0 +1,59 @@
+import { JwtPayload } from "jsonwebtoken";
+import { validateUser } from "../../../utils/index.js";
+import { Request, Response, NextFunction } from "express";
+import { assert } from "console";
+
+describe("validateUser", () => {
+  afterEach(() => {
+    jest.clearAllMocks(); // Clear any previous mocks to avoid interference between tests
+  });
+
+  it("should validate a user when the request id matches the user id", async () => {
+    const mockUser = {
+      id: "1",
+    } as JwtPayload;
+
+    const mockRequest = buildMockRequest(mockUser);
+    const mockResponse = new MockResponse() as unknown as Response;
+    const next = jest.fn();
+    validateUser(mockRequest, mockResponse, next);
+
+    expect(next).toHaveBeenCalled();
+  });
+
+  it("should fail when no user id", async () => {
+    const mockUser = {} as JwtPayload;
+
+    const mockRequest = buildMockRequest(mockUser);
+    const mockResponse = new MockResponse() as unknown as Response;
+    const next = jest.fn();
+    const actualResponse = await validateUser(mockRequest, mockResponse, next);
+
+    expect(actualResponse?.statusCode).toEqual(403);
+    expect(next).toHaveBeenCalledTimes(0);
+  });
+
+  // write POST test
+});
+
+function buildMockRequest(mockUser: JwtPayload) {
+  return {
+    params: {
+      id: 1,
+    },
+    user: mockUser,
+  } as unknown as Request;
+}
+
+class MockResponse {
+  statusCode: number = 0;
+
+  status(statusCode: number) {
+    this.statusCode = statusCode;
+    return this;
+  }
+
+  json(body: Object) {
+    return this;
+  }
+}
